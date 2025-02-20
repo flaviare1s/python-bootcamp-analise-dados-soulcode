4a8a55ce969e55cab64ddc03868142366e215cbc
flaviare1s <flaviarster@gmail.com>
2025-02-20 09:32:19 -0300

    Update sum.py

--- sum.py
+++ sum.py
@@ -9,18 +9,33 @@ print(sum(lista))
 
 # Escreva um programa que receba o número de dias de três atividades e exiba o tempo total do projeto. Se algum valor for negativo, mostre uma mensagem informando o erro.
 
-atividade_a = int(input('Digite o número de dias da primeira atividade: '))
-atividade_b = int(input('Digite o número de dias da segunda atividade: '))
-atividade_c = int(input('Digite o número de dias da terceira atividade: '))
+# atividade_a = int(input('Digite o número de dias da primeira atividade: '))
+# atividade_b = int(input('Digite o número de dias da segunda atividade: '))
+# atividade_c = int(input('Digite o número de dias da terceira atividade: '))
+
+# atividades = []
+
+# if atividade_a < 0 or atividade_b < 0 or atividade_c < 0:
+#   print('Os valores inseridos são inválidos')
+# else:
+#   atividades.append(atividade_a)
+#   atividades.append(atividade_b)
+#   atividades.append(atividade_c)
+
+#   total = sum(atividades)
+#   print(f'Tempo total do projeto: {total} dias')
+
+# Outra maneira de fazer:
 
 atividades = []
 
-if atividade_a < 0 or atividade_b < 0 or atividade_c < 0:
-  print('Os valores inseridos são inválidos')
-else:
-  atividades.append(atividade_a)
-  atividades.append(atividade_b)
-  atividades.append(atividade_c)
+for i in range(3):
+  valor = int(input(f'Digite o número de dias da atividade {i + 1}: '))
 
-  total = sum(atividades)
-  print(f'Tempo total do projeto: {total} dias')
+  if valor < 0:
+    print('Os valores inseridos são inválidos')
+  else:
+    atividades.append(valor)
+
+total = sum(atividades)
+print(f'Tempo total do projeto: {total} dias')

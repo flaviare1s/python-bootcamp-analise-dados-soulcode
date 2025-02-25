7171768ecc68708393b6ddf1365ea6961e63a0d0
flaviare1s <flaviarster@gmail.com>
2025-02-25 16:53:35 -0300

    Update arrays.py

--- NumPy/arrays.py
+++ NumPy/arrays.py
@@ -17,3 +17,36 @@ print(array.size) # Retorna a quantidade de elementos
 
 # Dtype
 print(array.dtype)
+
+# Slicing
+arr = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+array_slicing = arr[0:2]
+print(array_slicing)
+
+# Sum
+array_sum = arr.sum()
+print(array_sum)
+
+# Média aritmética
+array_mean = arr.mean()
+print(array_mean)
+
+# Mediana
+array_median = np.median(arr)
+print(array_median)
+
+# Valor máximo
+array_max = arr.max()
+print(array_max)
+
+# Valor mínimo
+array_min = arr.min()
+print(array_min)
+
+# Índice do maior valor
+array_argmax = arr.argmax()
+print(array_argmax)
+
+# Índice do menor valor
+array_argmin = arr.argmin()
+print(array_argmin)
